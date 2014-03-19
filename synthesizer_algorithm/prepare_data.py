592a1bcf6f348d2157128282308aa5ee6070555c
Conor Henley <cjhkb8@gmail.com>
2014-03-19 16:24:53 -0700

    changes to how unique ids are calculated for hh and persons

--- synthesizer_algorithm/prepare_data.py
+++ synthesizer_algorithm/prepare_data.py
@@ -5,6 +5,7 @@ import synthesizer_algorithm.adjusting_sample_joint_distribution as jd
 import drawing_households
 import synthesizer_algorithm.pseudo_sparse_matrix as ps
 import time
+import itertools
 from scipy import sparse
 
 # this function adds the unique_ids that were previously added by adjusting_sample_joint_distribution.create_update_string and add_unique_id
@@ -15,18 +16,32 @@ def prepare_data(data_dir, hh_sample_file, per_sample_file, hh_marginals_file, p
     hh_vars = np.array((hh_sample.columns)[4:]) # identifies the household control variables
     hh_var_list = list(hh_sample.columns[4:])
     hh_dims = np.array((hh_sample.max())[4:]) # identifies number of categories per household control variable
+    hh_var_dims = zip(hh_var_list, list(hh_dims))
     hhld_units = len(hh_sample.index)  # identifies the number of housing units to build the Master Matrix
     hh_sample['group_id'] = ''
     for var in hh_var_list:
         hh_sample[var + '_str'] = hh_sample[var].astype('str')
         hh_sample.group_id = hh_sample.group_id + hh_sample[var + '_str']
         hh_sample = hh_sample.drop([var + '_str'], axis=1)
-    hh_marginals = pd.read_csv(hh_marginals_file, header = 0)
-    hhid = hh_sample.groupby(['group_id'], as_index=False)['state'].min()
-    hhid['hhld_uniqueid'] = hhid.index + 1
-    hhid = hhid[['group_id', 'hhld_uniqueid']]
+    vals = list(hh_dims)
+    hhid_comb = []
+    for item in itertools.product(*[range(1,x+1) for x in vals]):
+        hhid_comb.append(item)
+    hhid_var = pd.DataFrame(data=hhid_comb)
+    hhid_var['hhld_uniqueid'] = hhid_var.index + 1
+    hhid_var['group_id'] = ''
+    for val in range(len(vals)):
+        hhid_var[str(val)+'_str'] = hhid_var[val].astype('str')
+        hhid_var['group_id'] = hhid_var['group_id'] + hhid_var[str(val)+'_str']
+        hhid_var = hhid_var.drop([str(val)+'_str'], axis=1)
+    hhid = hhid_var[['group_id','hhld_uniqueid']]
+    agg = dict.fromkeys(hh_var_list,'min')
+    #hhid_var = hh_sample.groupby(['group_id'], as_index=False).agg(agg)
+    #hhid_var['hhld_uniqueid'] = hhid_var.index + 1
+    #hhid = hhid_var[['group_id', 'hhld_uniqueid']]
     hh_sample = pd.merge(hh_sample, hhid, how='left', left_on='group_id', right_on='group_id')
     hh_sample = hh_sample.drop('group_id', axis=1)
+    hh_marginals = pd.read_csv(hh_marginals_file, header = 0)
 
     per_sample = pd.read_csv(per_sample_file, header = 0)
     per_vars = list(per_sample.columns)[5:] # identifies the person control variables
@@ -35,12 +50,25 @@ def prepare_data(data_dir, hh_sample_file, per_sample_file, hh_marginals_file, p
         per_sample[var + '_str'] = per_sample[var].astype('str')
         per_sample.group_id = per_sample.group_id + per_sample[var + '_str']
         per_sample = per_sample.drop([var + '_str'], axis=1)
-    pid = per_sample.groupby(['group_id'], as_index=False)['state'].min()
-    pid['person_uniqueid'] = pid.index + 1
-    pid = pid[['group_id', 'person_uniqueid']]
+    vals = list(per_dims)
+    per_comb = []
+    for item in itertools.product(*[range(1,x+1) for x in vals]):
+        per_comb.append(item)
+    pid_var = pd.DataFrame(data=per_comb)
+    pid_var['person_uniqueid'] = pid_var.index + 1
+    pid_var['group_id'] = ''
+    for val in range(len(vals)):
+        pid_var[str(val)+'_str'] = pid_var[val].astype('str')
+        pid_var['group_id'] = pid_var['group_id'] + pid_var[str(val)+'_str']
+        pid_var = pid_var.drop([str(val)+'_str'], axis=1)
+    pid = pid_var[['group_id','person_uniqueid']]
+    p_agg = dict.fromkeys(per_vars,'min')
+    #pid_var = per_sample.groupby(['group_id'], as_index=False).agg(p_agg)
+    #pid_var['person_uniqueid'] = pid_var.index + 1
+    #pid = pid_var[['group_id', 'person_uniqueid']]
     per_sample = pd.merge(per_sample, pid, how='left', left_on='group_id', right_on='group_id')
     per_sample = per_sample.drop('group_id', axis=1)
-    per_dims = np.array(per_sample.astype('int').max())[5:]
+    per_dims = np.array(per_sample.astype('int').max())[5:-1] # identifies number of categories per household control variable
     per_vars_dims = dict(zip(per_vars, per_dims))
     per_marginals = pd.read_csv(per_marginals_file, header = 0)
     
@@ -56,5 +84,5 @@ def prepare_data(data_dir, hh_sample_file, per_sample_file, hh_marginals_file, p
     hhld_0_joint_dist = jd.hhld_0_joint_dist(hh_sample, hh_var_list)
     person_0_joint_dist = jd.person_0_joint_dist(per_sample, per_vars)
 
-    return {'matrix':matrix, 'sparse_matrix':sparse_matrix, 'sparse_matrix1':sparse_matrix, 'index_matrix':index_matrix, 'housing_synthetic_data':housing_synthetic_data, 'person_synthetic_data':person_synthetic_data, 'hhld_0_joint_dist':hhld_0_joint_dist, 'person_0_joint_dist':person_0_joint_dist}
+    return {'matrix':matrix, 'sparse_matrix':sparse_matrix, 'sparse_matrix1':sparse_matrix1, 'index_matrix':index_matrix, 'housing_synthetic_data':housing_synthetic_data, 'person_synthetic_data':person_synthetic_data, 'hhld_0_joint_dist':hhld_0_joint_dist, 'person_0_joint_dist':person_0_joint_dist}
 
