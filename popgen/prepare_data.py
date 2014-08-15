961101673b77844f689058ca6f55f39ce23c96ed
Matt Davis <jiffyclub@gmail.com>
2014-08-15 16:44:07 -0700

    PEP8 fixes.

--- popgen/prepare_data.py
+++ popgen/prepare_data.py
@@ -5,14 +5,13 @@ import numpy as np
 import pandas as pd
 
 
-def prepare_data(data_dir, hh_sample_file, hh_marginals_file, per_sample_file, per_marginals_file):
+def prepare_data(data_dir, hh_sample_file, hh_marginals_file, per_sample_file,
+                 per_marginals_file):
     os.chdir(data_dir)
-    hh_sample = pd.read_csv(hh_sample_file, header = 0)
+    hh_sample = pd.read_csv(hh_sample_file, header=0)
     hh_sample = hh_sample.astype('int')
-    hh_vars = np.array((hh_sample.columns)[4:])
     hh_var_list = list(hh_sample.columns[4:])
     hh_dims = np.array((hh_sample.max())[4:])
-    hh_var_dims = zip(hh_var_list, list(hh_dims))
     hhld_units = len(hh_sample.index)
     hh_sample['group_id'] = ''
     for var in hh_var_list:
@@ -21,7 +20,7 @@ def prepare_data(data_dir, hh_sample_file, hh_marginals_file, per_sample_file, p
         hh_sample = hh_sample.drop([var + '_str'], axis=1)
     vals = list(hh_dims)
     hhid_comb = []
-    for item in itertools.product(*[range(1,x+1) for x in vals]):
+    for item in itertools.product(*[range(1, x + 1) for x in vals]):
         hhid_comb.append(item)
     hhid_var = pd.DataFrame(data=hhid_comb, columns=hh_var_list)
     hhid_var['hhld_uniqueid'] = hhid_var.index + 1
@@ -30,12 +29,12 @@ def prepare_data(data_dir, hh_sample_file, hh_marginals_file, per_sample_file, p
         hhid_var[val+'_str'] = hhid_var[val].astype('str')
         hhid_var['group_id'] = hhid_var['group_id'] + hhid_var[val+'_str']
         hhid_var = hhid_var.drop([val+'_str'], axis=1)
-    hhid = hhid_var[['group_id','hhld_uniqueid']]
-    agg = dict.fromkeys(hh_var_list,'min')
-    hh_sample = pd.merge(hh_sample, hhid, how='left', left_on='group_id', right_on='group_id')
-    hh_marginals = pd.read_csv(hh_marginals_file, header = 0)
+    hhid = hhid_var[['group_id', 'hhld_uniqueid']]
+    hh_sample = pd.merge(
+        hh_sample, hhid, how='left', left_on='group_id', right_on='group_id')
+    hh_marginals = pd.read_csv(hh_marginals_file, header=0)
 
-    per_sample = pd.read_csv(per_sample_file, header = 0)
+    per_sample = pd.read_csv(per_sample_file, header=0)
     per_vars = list(per_sample.columns)[5:]
     per_dims = np.array(per_sample.astype('int').max())[5:]
     per_sample['group_id'] = ''
@@ -45,7 +44,7 @@ def prepare_data(data_dir, hh_sample_file, hh_marginals_file, per_sample_file, p
         per_sample = per_sample.drop([var + '_str'], axis=1)
     vals = list(per_dims)
     per_comb = []
-    for item in itertools.product(*[range(1,x+1) for x in vals]):
+    for item in itertools.product(*[range(1, x + 1) for x in vals]):
         per_comb.append(item)
     pid_var = pd.DataFrame(data=per_comb, columns=per_vars)
     pid_var['person_uniqueid'] = pid_var.index + 1
@@ -54,20 +53,22 @@ def prepare_data(data_dir, hh_sample_file, hh_marginals_file, per_sample_file, p
         pid_var[val+'_str'] = pid_var[val].astype('str')
         pid_var['group_id'] = pid_var['group_id'] + pid_var[val+'_str']
         pid_var = pid_var.drop([val+'_str'], axis=1)
-    pid = pid_var[['group_id','person_uniqueid']]
-    p_agg = dict.fromkeys(per_vars,'min')
-    per_sample = pd.merge(per_sample, pid, how='left', left_on='group_id', right_on='group_id')
-    per_vars_dims = dict(zip(per_vars, per_dims))
-    per_marginals = pd.read_csv(per_marginals_file, header = 0)
-    return hh_sample, hh_marginals, per_sample, per_marginals, hh_var_list, per_vars, hhld_units, hh_dims, per_dims, hhid_var, pid_var
-    
+    pid = pid_var[['group_id', 'person_uniqueid']]
+    per_sample = pd.merge(
+        per_sample, pid, how='left', left_on='group_id', right_on='group_id')
+    per_marginals = pd.read_csv(per_marginals_file, header=0)
+    return (
+        hh_sample, hh_marginals, per_sample, per_marginals, hh_var_list,
+        per_vars, hhld_units, hh_dims, per_dims, hhid_var, pid_var)
+
+
 def variables(hh_var_list, hh_dims, per_vars, per_dims):
     hh_varcorrdict = {}
     for var in range(len(hh_var_list)):
-        for i in range(1,hh_dims[var]+1):
-            hh_varcorrdict[hh_var_list[var]+str(i)] = [hh_var_list[var], i]
+        for i in range(1, hh_dims[var] + 1):
+            hh_varcorrdict[hh_var_list[var] + str(i)] = [hh_var_list[var], i]
     per_varcorrdict = {}
     for var in range(len(per_vars)):
-        for i in range(1,per_dims[var]+1):
-            per_varcorrdict[per_vars[var]+str(i)] = [per_vars[var], i]
+        for i in range(1, per_dims[var] + 1):
+            per_varcorrdict[per_vars[var] + str(i)] = [per_vars[var], i]
     return hh_varcorrdict, per_varcorrdict
