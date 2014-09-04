5626ce3617043bdd45eb6e50f8014ac871ac542d
Fletcher Foti <fscottfoti@gmail.com>
2014-09-04 11:59:55 -0700

    pep8

--- synthpop/recipes/tests/test_starter.py
+++ synthpop/recipes/tests/test_starter.py
@@ -2,9 +2,11 @@ import pytest
 from ..starter import Starter
 from ...synthesizer import SynthPop
 
+
 @pytest.fixture
 def key():
     return "827402c2958dcf515e4480b7b2bb93d1025f9389"
+
 
 def test_starter(key):
     st = Starter(key, "CA", "Napa County")
