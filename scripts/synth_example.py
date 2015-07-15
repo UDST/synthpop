f2028ec8f72d36e8a12cc28278922e6fa05d37aa
unknown <eddie.janowicz@gmail.com>
2015-07-15 09:20:32 -0700

    alternative reciple example

--- scripts/synth_example.py
+++ scripts/synth_example.py
@@ -1,10 +1,10 @@
-from synthpop.recipes.starter import Starter
+from synthpop.recipes.starter2 import Starter
 from synthpop.synthesizer import synthesize_all, enable_logging 
 import os
 
 def synthesize_county(county):
-    starter = Starter(os.environ["CENSUS"], "CA", county)
+    starter = Starter(os.environ["CENSUS"], "CO", county)
     synthetic_population = synthesize_all(starter)
     return synthetic_population
     
-synthesize_county('Solano County')
+synthesize_county('Gilpin County')
