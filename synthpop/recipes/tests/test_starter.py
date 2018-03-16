b9d250fb1efb28ec0cae1c102d7bad654a11fd8e
cvanoli <catalinavanoli@berkeley.edu>
2018-03-16 18:39:10 -0300

    uncommented test_starter to test starter2

--- synthpop/recipes/tests/test_starter.py
+++ synthpop/recipes/tests/test_starter.py
@@ -1,7 +1,7 @@
 import pytest
 from ...synthesizer import *
-from ..starter2 import Starter
-# from ..starter2 import Starter as Starter2
+# from ..starter import Starter
+from ..starter2 import Starter as Starter2
 
 
 @pytest.fixture
@@ -9,14 +9,14 @@ def key():
     return "827402c2958dcf515e4480b7b2bb93d1025f9389"
 
 
-def test_starter(key):
-    st = Starter(key, "CA", "Napa County")
-    # just run it for now
-    synthesize_all(st, num_geogs=1)
+# def test_starter(key):
+#     st = Starter(key, "CA", "Napa County")
+#     # just run it for now
+#     synthesize_all(st, num_geogs=1)
 
 
 # commented out as it is to slow for travis
-# def test_starter2(key):
-#     st = Starter2(key, "CA", "Napa County")
-#     # just run it for now
-#     synthesize_all(st, num_geogs=1)
+def test_starter2(key):
+    st = Starter2(key, "CA", "Napa County")
+    # just run it for now
+    synthesize_all(st, num_geogs=1)
