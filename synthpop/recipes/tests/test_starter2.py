df75c366e40d75a9b48b1fb4c2358a8f979ea28b
cvanoli <catalinavanoli@berkeley.edu>
2018-04-27 20:18:32 -0300

    changed census Key in test_starter

--- synthpop/recipes/tests/test_starter2.py
+++ synthpop/recipes/tests/test_starter2.py
@@ -5,7 +5,7 @@ from ..starter2 import Starter as Starter2
 
 @pytest.fixture
 def key():
-    return "827402c2958dcf515e4480b7b2bb93d1025f9389"
+    return "bfa6b4e541243011fab6307a31aed9e91015ba90"
 
 
 def test_starter2(key):
