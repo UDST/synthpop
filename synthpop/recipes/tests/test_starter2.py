5ab47996db004089f07b744f8c9cd91e2288da67
cvanoli <catalinavanoli@berkeley.edu>
2019-07-15 11:32:34 -0700

    starter2 parallel test

---
+++ synthpop/recipes/tests/test_starter2.py
@@ -0,0 +1,22 @@
+import pytest
+
+from synthpop.synthesizer import *
+from synthpop.recipes.starter import Starter
+from synthpop.recipes.starter2 import Starter as Starter2
+
+
+@pytest.fixture
+def key():
+    return "827402c2958dcf515e4480b7b2bb93d1025f9389"
+
+
+def test_starter(key):
+    st = Starter(key, "IL", "Kendall County")
+    # just run it for now
+    synthesize_all(st, num_geogs=1)
+
+
+# no synthesizer bc it's too memory intensive for travis
+def test_starter2(key):
+    Starter2(key, "IL", "Kendall County")
+    synthesize_all_in_parallel(st, num_geogs=1) 
